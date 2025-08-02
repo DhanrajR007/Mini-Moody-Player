655deac6e4b2dbe8025d2f301faf4fab42037ed7
DhanrajR007 <dhanrajrathore1111@gmail.com>
2025-08-02 15:27:13 +0530

    adding a imagekit

---
+++ backend/src/routes/song.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const uploadFile = require("../service/storage.service");
+const song = require("../models/song.model");
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.post("/songs", upload.single("audio"), async(req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+
+  const fileData= await uploadFile(req.file);
+  console.log(fileData)
+ res.status(201).json(({
+    message:"song created successfully",
+    song:req.body
+ }))
+});
+
+module.exports = router;
